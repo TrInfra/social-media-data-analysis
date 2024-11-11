8ee0da61c66d7043373e1b4475ef711c69eb68ad
socorro bastos <srdesajustado1416@gmail.com>
2024-11-11 20:11:11 -0300

    refactor(sentiment_analysis)!: modify the logic so that the polarity matches the name of the songs

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -8,26 +8,27 @@ from textblob import TextBlob
 dir = Path('./Data/Processed')
 
 num_files = 0
-polarities = []
-subjectivities = []
+polarities = {} 
+subjectivities = {} 
 
 for file in dir.iterdir():
     with open(file, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
     text = data.get('lyrics', '')  
+    music_name = file.stem 
 
     blob = TextBlob(text)
     sentiment = blob.sentiment
     print(f"File: {file.name} - Polarity: {sentiment.polarity}, Subjectivity: {sentiment.subjectivity}")
     
-    polarities.append(sentiment.polarity)
-    subjectivities.append(sentiment.subjectivity)
+    polarities[music_name] = sentiment.polarity
+    subjectivities[music_name] = sentiment.subjectivity
     num_files += 1
 
 if num_files > 0:
-    avg_polarity = sum(polarities) / num_files
-    avg_subjectivity = sum(subjectivities) / num_files
+    avg_polarity = sum(polarities.values()) / num_files
+    avg_subjectivity = sum(subjectivities.values()) / num_files
     print(f"\nAverage Polarity: {avg_polarity}, Average Subjectivity: {avg_subjectivity}")
 else:
     print("No JSON files found.")
